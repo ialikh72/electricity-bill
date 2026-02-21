86091d590bb82bbf90dccecf2cfd76d4d87b53e7
ialikh72 <ialikh.w1@gmail.com>
2026-02-21 20:09:59 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("enter units:");
+        double units = double.Parse(Console.ReadLine());
+        double bill = 0;
+        if (units <= 50)
+        {
+            bill = units * 0.5;
+        }
+        else if (units <= 100)
+        {
+            bill = 50 * 0.5 + (units - 50) * 0.75;
+        }
+        else
+        {
+            bill = 50 * 0.5 + 50 * 0.75 + (units -100)* 1;
+        }
+       
+        Console.WriteLine("total bill is:"+bill);
+    }
+}
